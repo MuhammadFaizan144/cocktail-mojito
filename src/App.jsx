4b22a0946a74208f74615bcfcfa0b2c957959ebd
MuhammadFaizan144 <fg7829098@gmail.com>
2026-02-24 16:00:43 +0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import gsap from 'gsap';
 import { ScrollTrigger ,SplitText} from 'gsap/all'
 import React from 'react'
 gsap.registerPlugin(ScrollTrigger,SplitText);
-
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <h1 className='flex-center text-2xl h-screen'>
-      denjlk
-    </h1>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
