5b79213cb7a0e356d139aca875387fa330731171
MuhammadFaizan144 <fg7829098@gmail.com>
2026-02-28 11:22:06 +0500

    menu section

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -13,7 +13,7 @@ const Art = () => {
                 trigger:'#art',
                 start,
                 end:'bottom center',
-                scrub:0.15,
+                scrub:1.5,
                 pin:true,
 
             }
